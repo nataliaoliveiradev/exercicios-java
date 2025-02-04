bdb0fcde7bd7b215030d5847ca4dd10be92a5136
nataliaoliveiradev <profissionalnataliaoliveira@gmail.com>
2025-02-04 17:31:07 -0300

    Jogo de adivinhação e Array Lists

--- src/empresa/Leonidas.java
+++ src/empresa/Leonidas.java
@@ -1,4 +1,24 @@
 package empresa;
 
+import java.util.Scanner;
+
 public class Leonidas {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite seu palpite:");
+        int palpite = teclado.nextInt();
+
+        while (palpite != 10000) {
+            if (palpite < 10000) {
+                System.out.println("Um pouco mais...");
+            } else {
+                System.out.println("Um pouco menos...");
+            }
+            System.out.println("Digite seu próximo palpite");
+            palpite = teclado.nextInt();
+        }
+        System.out.println("Seu palpite está correto");
+
+    }
 }
